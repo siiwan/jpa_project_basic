21d2708b004f8802607fee163cf39eb312e8fe8d
gsw <ggssww99@gmail.com>
2023-08-26 10:30:35 +0900

    기본 키 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,57 +1,45 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
+//@SequenceGenerator(name="MEMBER_SEQ_GENERATOR", sequenceName = "MEMBER_SEQ")
+//@TableGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES",
+//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE)
+//    @GeneratedValue(strategy = GenerationType.TABLE,
+//            generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     //insertable, updatable : 등록, 변경 가능 여부
     // nullable(DDL) : null 값의 허용 여부를 설정한다. false로 설정하면 DDL 생성 시에 not null 제약조건이 붙는다.
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    private String username;
+    public Long getId() {
+        return id;
+    }
 
-    private Integer age;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    // @Enumerated - JAVA에서 enum타입을 사용하기 위해 사용
-    // 주의! ORDINAL 사용X, 무조건 STRING으로
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    public String getName() {
+        return name;
+    }
 
-    // 시간을 사용하기 위해 사용
-    // Temporal : DATE, TIME, TIMESTAMP
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    // DB의 형식이 date
-    //private LocalDate testLocalDate;
-
-    // DB의 형식이 timestamp
-    //private LocalDateTime testLocalDateTime;
-
-    // db의 clob
-    // 데이터베이스 BLOB, CLOB 타입과 매핑
-    // @Lob에는 지정할 수 있는 속성이 없다.
-    // 매핑하는 필드 타입이 문자면 CLOB 매핑, 나머지는 BLOB 매핑
-    // CLOB: String, char[], java.sql.CLOB
-    // BLOB: byte[], java.sql. BLOB
-    @Lob
-    private String description;
-
-    // 특정 필드를 컬럼에 매핑하지 않음(매핑 무시)
-    // 캐시에만 남아있음
-//    @Transient
-//    private int temp;
-
-    //JPA는 기본생성자가 있어야함
-    public Member() {
+    public void setName(String name) {
+        this.name = name;
     }
 }
