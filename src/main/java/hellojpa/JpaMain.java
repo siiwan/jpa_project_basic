118f0ca9c6f038b55d1f318997b9e8f952dfc593
gsw <ggssww99@gmail.com>
2023-09-24 09:26:04 +0900

    영속성 전이(CASCADE)와 고아 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,46 +20,21 @@ public class JpaMain {
 
         try {
 
-            Team team1 = new Team();
-            team1.setName("teamA");
-            em.persist(team1);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
-
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team1);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
-//            Member member2 = new Member();
-//            member2.setUsername("member2");
-//            em.persist(member2);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
 
             em.flush();
             em.clear();
 
-            //Member m1 = em.find(Member.class, member1.getId());
-            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
-
-
-            System.out.println("===================");
-//            m1.getTeam();
-//            m1.getTeam().getName(); //초기화 (이때 데이터 조회 lazy인 경우)
-            System.out.println("===================");
-
-
-//            Member m1 = em.find(Member.class, member1.getId());
-//            Member m2 = em.getReference(Member.class, member2.getId());
-//            logic(m1, m2);
-
-
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
             // commit한 시점에 영속성 컨텍스트에 있는 쿼리가 실행
             tx.commit();
         } catch (Exception e){
@@ -70,11 +45,5 @@ public class JpaMain {
         emf.close();
     }
 
-    private static void logic(Member m1, Member m2) {
-        // 타입비교 하기 instanceof으로 하기
-        System.out.println("(m1.getClass() == m2.getClass()) = " + (m1.getClass() == m2.getClass()));
-        System.out.println("(m1 instanceof Member) = " + (m1 instanceof Member));
-        System.out.println("(m2 instanceof Member) = " + (m2 instanceof Member));
-    }
 
 }
